71b03fafe817c889813015c1d6ca5d297bb4b3ef
embyr333 <emb333@gmail.com>
2024-10-16 18:44:43 +0100

    Iteration2

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 def upper_SQL_keywords(s: str) -> str: # Note1
-    mod_s = s.replace('select', 'SELECT')
-    mod_s = mod_s.replace('from', 'FROM')
-    mod_s = mod_s.replace('where', 'WHERE')
-    mod_s = mod_s.replace('is', 'IS')
-    return mod_s
+    key_set = ('select', 'from', 'where', 'is')
+    word_list = s.split()
+    for i in range(len(word_list)):
+        if word_list[i] in key_set:
+            word_list[i] = word_list[i].upper()
+    mod_query = ' '.join(word_list)
+    return mod_query
 
 # TODO: Try to deal with posibility of double-quotes being used in query etc?
+# (would have to change 
+# ...and also that keywords might be used within a suted string in query, where DON'T want to mod
 
 
-print(upper_SQL_keywords("select first_name, last_name, gender from patients where gender is 'M';"))
+print(upper_SQL_keywords("select first_name, last_name, gender from patients where gender is M';"))
 
 
 # Note1: using the optional arg and return type specifiers has the advantage
